6b18fb1fb64e1128ffc6bd197369e8174746be1e
Tanawat Tassana <amp.tanawat@gmail.com>
2016-10-15 03:46:05 +0700

    Render simple React component

---
+++ src/app.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+    <section>
+        <h1>My Application</h1>
+    </section>
+)
+
+ReactDOM.render(<App />, document.getElementById('app'))
