d587111d3c5a64d62a821f7bb23d530a33e046f2
Tanawat Tassana <amp.tanawat@gmail.com>
2016-10-21 20:36:50 +0700

    Layout movie list

--- src/app.js
+++ src/app.js
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => (
-    <section>
-        <h1>Movie Collection</h1>
-    </section>
+const SearchForm = () => (
+    <form>
+        <input type="text" />
+        <button>Search</button>
+    </form>
 )
+
+const MovieList = (props) => (
+    <ul>
+    {props.movies.map(movie => {
+        return (
+            <li>{movie.title}</li>
+        )
+    })}
+    </ul>
+)
+
+const App = () => {
+    const movies = [
+        {
+            title: 'Rogue One: A Star Wars Story'
+        },
+        {
+            title: 'Guardians of the Galaxy Vol. 2'
+        },
+        {
+            title: 'Doctor Strange'
+        }
+    ]
+    return (
+        <section>
+            <h1>Movie Collection</h1>
+            <SearchForm />
+            <MovieList movies={movies} />
+        </section>
+    )
+}
 
 ReactDOM.render(<App />, document.getElementById('app'))
