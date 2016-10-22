0a2bc9feb9c157e7fbe7a7fe37846a0ba1a7cd7c
sinapam <sinapam@proteus-tech.com>
2016-10-22 11:07:05 +0700

    extract search form to its own file

--- src/app.js
+++ src/app.js
@@ -1,35 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-class SearchForm extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            query: ''
-        }
-    }
-    onSearchClick(event) {
-        event.preventDefault()
-        console.log('this.onSearchClick', this.state.query)
-    }
-    onQueryChange(event) {
-        const query = event.target.value
-        console.log('this.onQueryChange', query)
-        this.setState({
-            query: query
-        })
-    }
-    render() {
-        return (
-            <form>
-                <input type="text" 
-                    value={this.state.query} 
-                    onChange={this.onQueryChange.bind(this)} />
-                <button onClick={this.onSearchClick.bind(this)}>Search</button>
-            </form>
-        )
-    }
-}
+import { SearchForm } from './search-form'
 
 const MovieList = (props) => (
     <ul>
